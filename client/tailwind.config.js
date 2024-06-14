4fd32210790c61c4a458d492b1251e4790354ca3
Maxim Osin <maxim.neitralov@gmail.com>
2024-06-14 13:53:05 +0700

    Подключен шрифт Ubuntu

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,30 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+    screens: {
+      "sm": "480px",
+      "md": "768px",
+      "lg": "960px",
+      "xl": "1280px"
+    },
+    extend: {
+      colors: {
+        "primary": "#fb8b24",
+        "dark-primary": "#f98212",
+        "neutral": "#ffffff",
+        "dark-neutral": "#fafafa",
+        "second": "#d4cfcc",
+        "attention": "#f54949",
+        "muted": "#454545",
+        "background": "#f4f4f4"
+      },
+      fontFamily: {
+        "sans": "Ubuntu"
+      },
+    },
   },
   plugins: [],
 }
