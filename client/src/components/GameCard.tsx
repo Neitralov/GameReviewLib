1e3a4963b95f92481b565ca0f5fd9bb0c97ef609
Maxim Osin <maxim.neitralov@gmail.com>
2024-06-29 18:28:47 +0700

    Исправлен адрес

--- client/src/components/GameCard.tsx
+++ client/src/components/GameCard.tsx
@@ -10,7 +10,7 @@ export const GameCard:FC<Props> = ({review, onClick}) => {
   return (
     <div
       className={"flex flex-col-reverse min-w-[150px] w-[150px] h-[225px] px-3 py-1.5 bg-cover shadow-[inset_0_-35px_12px_-12px_rgba(0,0,0,35%)] rounded-lg cursor-pointer snap-start"}
-      style={{backgroundImage: `url('http://localhost:8081/${review.posterPath}')`}}
+      style={{backgroundImage: `url('http://localhost:7432/${review.posterPath}')`}}
       onClick={() => onClick(review)}>
       <div className={"flex justify-between"}>
         <p className={"text-sm text-white select-none"}>{review.releaseYear}</p>
