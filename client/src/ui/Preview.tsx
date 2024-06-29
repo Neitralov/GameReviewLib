4f92d11e889cf5908cb0df819203b63c03effd6d
Maxim Osin <maxim.neitralov@gmail.com>
2024-06-29 19:30:43 +0700

    Добавлена серая рамка для превью в редакторе, как у карточки в сетке

--- client/src/ui/Preview.tsx
+++ client/src/ui/Preview.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const Preview:FC<Props> = ({imageUrl}) => {
   return (
     <div
-      className={"min-w-[180px] w-[180px] h-[270px] bg-cover rounded-lg"}
+      className={"min-w-[180px] w-[180px] h-[270px] bg-cover bg-center border-solid border border-gray-300 rounded-lg"}
       style={{backgroundImage: `url('${imageUrl}')`}}>
     </div>
   )
